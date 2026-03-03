617a6b65ea939e7ae1280ea9aa3c33e471407e51
AnilVcodehax <anil.karlapudi@infovision.com>
2026-03-03 16:50:33 +0530

    Update conftest and logger files safely

--- conftest.py
+++ conftest.py
@@ -1,4 +1,24 @@
 import sys
 import os
+import pytest
+import allure
+from selenium import webdriver
+
 
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+
+    if report.when == "call" and report.failed:
+        driver = item.funcargs.get("driver", None)
+        if driver:
+            screenshot = driver.get_screenshot_as_png()
+            allure.attach(
+                screenshot,
+                name="Failure Screenshot",
+                attachment_type=allure.attachment_type.PNG
+            )
